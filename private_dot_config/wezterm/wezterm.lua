5740b95a34ae813c941f191f3781c76217ecdb7b
Vladimir Ivakin <vla.ivakin@gmail.com>
2024-04-05 15:52:59 +0200

    Add wezterm config

---
+++ private_dot_config/wezterm/wezterm.lua
@@ -0,0 +1,45 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.color_scheme = 'Dracula+'
+
+config.window_decorations = 'RESIZE'
+
+config.font = wezterm.font 'Iosevka'
+config.font_size = 14
+
+config.keys = { {
+    key = 'd',
+    mods = 'CMD',
+    action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }
+}, {
+    key = 'd',
+    mods = 'CMD|SHIFT',
+    action = act.SplitVertical { domain = 'CurrentPaneDomain' }
+}, {
+    key = 'LeftArrow',
+    mods = 'OPT',
+    action = act.SendKey { key = 'b', mods = 'ALT' }
+}, {
+    key = 'RightArrow',
+    mods = 'OPT',
+    action = act.SendKey { key = 'f', mods = 'ALT' }
+}, {
+    key = 'R',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+        description = wezterm.format { { Attribute = { Intensity = 'Bold' } }, { Text = 'Enter tab name:' } },
+        action = wezterm.action_callback(function(window, pane, line)
+            -- line will be `nil` if they hit escape without entering anything
+            -- An empty string if they just hit enter
+            -- Or the actual line of text they wrote
+            if line then
+                window:active_tab():set_title(line)
+            end
+        end),
+    },
+} }
+
+return config
